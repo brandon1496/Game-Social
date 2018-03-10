3a2f3ab23a8b99f41bf16f2eab4e612cfd59bcb1
Brandon Innis <brandoninnis96@gmail.com>
2018-03-10 18:42:38 -0500

    Worked on the group app's templates, views, urls and admin

---
+++ groups/urls.py
@@ -0,0 +1,10 @@
+from django.conf.urls import url
+from django.contrib.auth import views as auth_views
+from django.urls import path
+from . import views
+
+app_name = 'groups'
+
+path(' ', views.ListGroup.as_view(), name='all'),
+path('new/', views.CreateGroup.as_view(), name='create'),
+path('posts/in/<slug:slug>/', views.SingleGroup.as_view(), name='single'),
--- groups/views.py
+++ groups/views.py
@@ -1,3 +1,18 @@
 from django.shortcuts import render
+from django.contrib.auth.mixins import (LoginRequiredMixin, PermissionRequiredMixin)
+
+from django.utils import reverse
+from django.views import generic
+from groups.models import Group, GroupMember
 
 # Create your views here.
+# Class to create a group
+class CreateGroup(LoginRequiredMixin, generic.CreateView):
+    fields = ('name', 'message')
+    model = Group
+
+class SingleGroup(generic.DetailView):
+    model = Group
+# List out the groups
+class ListGroup(generic.ListView):
+    model = Group
