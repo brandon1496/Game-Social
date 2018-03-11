d73d33062ec847d4ef7b329beea26cedfef5de7c
Brandon Innis <brandoninnis96@gmail.com>
2018-03-11 18:37:31 -0400

    Made changes to the Post/views and the posts/urls also added the apps to my project settings file

--- gamesocial/settings.py
+++ gamesocial/settings.py
@@ -39,6 +39,9 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'bootstrap4',
     'accounts',
+    'posts',
+    'groups',
+    'django-braces',
 ]
 
 MIDDLEWARE = [
--- posts/views.py
+++ posts/views.py
@@ -1,1 +1,72 @@
 from django.shortcuts import render
+from django.contrib.auth.mixins import LoginRequiredMixin
+from django.utils import reverse_lazy
+from django.views import generic
+from django.http import Http404
+from braces.views import SelectRelatedMixin
+from . import models
+from . import forms
+from django.contrib.auth import get_user_model
+User = get_user_model()
+
+# Create your views here.
+class PostList(SelectRelatedMixin, generic.ListView):
+    model = models.Post
+    #Shows the posts related to the user or group
+    select_related = ('user', 'group')
+
+class UserPosts(generic.ListView):
+    model = models.Post
+    template_name= "posts/UserPost_list.html"
+
+    #gets username of whoever is currently signed in right now 
+    def get_queryset(self):
+        try:
+            self.post_user = User.objects.prefetch_related('posts').get(username__iexact=self.kwargs.get('username'))
+        execpt User.DoesNotExist:
+            raise Http404
+        else:
+            return self.post_user.posts.all()
+    
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['post_user'] = self.post_user
+        return context
+
+
+class PostDetail(SelectRelatedMixin, generic.DetailView):
+    model = models.Post
+    select_related = ('user', 'group')
+
+    def get_queryset(self):
+        queryset = super().get_queryset()
+        # passes the username of the selected post into the DetailView
+        return queryset.filter(user_username__isexact=self.kwargs.get('username'))
+
+# Class to create posts
+class CreatePost(LoginRequiredMixin, SelectRelatedMixin, generic.CreateView):
+    fields = ('message', 'group')
+    model = models.Post
+    
+    
+    #overriding the form_valid function making sure that the new post is connected to the user that made it
+    def form_valid(self, form):
+        self.object = form.save(commit=False)
+        self.object.user = self.request.user
+        self.object.save()
+        return super().form_valid(form)
+
+# Class to Delete posts
+class DeletePost(LoginRequiredMixin, SelectRelatedMixin, generic.DeleteView):
+    model = models.Post
+    select_related = ('user', 'group')
+    # after post is deleted it will send the user to posts:all
+    success_url = reverse_lazy('posts:all')
+
+    def get_queryset(self):
+        queryset = super().get_queryset()
+        return queryset.filter(user_id= self.request.user.id)
+    # Will display to the  user that the Post was Deleted
+    def delete(self, *args, **kwargs):
+        messages.success(self.request, 'Post Deleted')
+        return super().delete(*args, **kwargs)
